903602e764e1eb9ca1ef35d03418919714d4c1b8
Priyansh <Priyanshyadav1012@gmail.com>
2024-07-21 17:14:09 +0530

    Footer and PreFooter Done.

--- src/Components/PreFooter.jsx
+++ src/Components/PreFooter.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Prefooter = () => {
   return (
-    <footer className='h-[310px] flex flex-1 items-center justify-center space-x-10'>
+    <footer className='h-[310px] flex flex-1 items-center justify-around space-x-10'>
       <div>
         <img src="./images/logo/Logo.png" alt="logo" className="logo" />
       </div>
 
       <div>
         <h5>Numărul de contac</h5>
-        <div className="flex flex-1 space-x-2">
+        <div className="flex flex-1 space-x-2 items-center justify-center">
           <img src="./images/vector/ic_round-phone.png" alt="phone" />
           <p><b>+ (373) 79 812 818</b></p>
         </div>
@@ -17,7 +17,7 @@ const Prefooter = () => {
 
       <div>
         <h5>E-mail</h5>
-        <div className="flex flex-1">
+        <div className="flex flex-1 space-x-2 items-center justify-center">
           <img src="./images/vector/material-symbols-light_mail.png" alt="mail" />
           <p><b>bsvdistribution@gmail.com</b></p>
         </div>
