b8f7c90287aa7e03369dc1a4f0eeecdba9cb23be
Priyansh <Priyanshyadav1012@gmail.com>
2024-07-21 17:50:31 +0530

    Livrare Section

--- src/Components/Livrare.jsx
+++ src/Components/Livrare.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 const Livarae = () => {
   return (
     <section className='flex-col items-center text-center'>
-      <h1 className="font-myriad text-[70px] font-bold leading-[84px] tracking-[0.06em] text-[#47174C]">
+      <h1 className="font-myriad text-[70px] font-bold leading-[84px] tracking-[0.06em] text-[#47174C] pb-20">
         Livrare și achitare
       </h1>
       <p className="font-inter text-[22px] font-medium leading-[26.63px] tracking-[0.06em] text-[#47174C]">
-        Suntem orientați spre comoditatea clienților noștri, de aceea <br />
+        Suntem orientați spre comoditatea clienților noștri, de aceea
         oferim opțiuni flexibile de achitare și primire a produsului nostru.
       </p>
 
-      <div className="flex flex-1 justify-center pt-40 px-20 space-x-2">
+      <div className="flex flex-1 justify-center pt-20 px-20 space-x-2">
         <div className="flex flex-col items-center w-[496px] h-[272px] bg-[#47174C2E] justify-center px-12 rounded-md">
           <img src="./images/vector/cart.png" alt="cart" className="h-[90px] w-[90px] mb-3"/>
           <h4 class="font-myriad text-[28px] leading-[33.6px] tracking-[0.06em] text-center">COMANDA</h4>
